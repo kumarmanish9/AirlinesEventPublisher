3d6d3660ec8c1954306ab58a93fab98887136b79
Manish Kumar <engmanishk@gmail.com>
2024-12-29 18:15:15 +0530

    Publishees made as per BRE

--- src/AirlinesEventPublisher/Program.cs
+++ src/AirlinesEventPublisher/Program.cs
@@ -2,8 +2,12 @@ using AirlineCoreLibrary.Utility;
 using AirlinesEventPublisher;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// Register the Core Library services
 builder.Services.RegisterAppServices();
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
+
 host.Run();
